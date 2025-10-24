dbdbd36eb8428b733b986bda30aa0170a8fbd4c9
Oljake <109909586+Oljake@users.noreply.github.com>
2025-10-24 19:51:45 +0300

    Delete constants.py

--- constants.py
+++
@@ -1,6 +0,0 @@
-# Text colors
-entry_ok        = "#005726"     # Darker green
-entry_warning   = "#008037"     # Lighter green
-entry_empty     = "#B3861D"     # Orange
-entry_error     = "#A1442A"     # Redish
-
