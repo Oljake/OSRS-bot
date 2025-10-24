97cd0cbdb642d2110a92b90e5c5eb32c5aab5ab6
Oljake <109909586+Oljake@users.noreply.github.com>
2025-10-24 13:22:56 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+# Text colors
+entry_ok        = "#005726"     # Darker green
+entry_warning   = "#008037"     # Lighter green
+entry_empty     = "#B3861D"     # Orange
+entry_error     = "#A1442A"     # Redish
+
